7a6815a69e1e32d6350667812dd026e67289583f
David VanScott <david.vanscott@gmail.com>
2014-07-29 16:08:17 -0400

    Update the database config

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,21 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_NAME'],
+			'username'  => $_ENV['DB_USERNAME'],
+			'password'  => $_ENV['DB_PASSWORD'],
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => 'help_',
+		),
+
+		'anodyneUsers' => array(
+			'driver'    => 'mysql',
+			'host'      => $_ENV['DB_USERS_HOST'],
+			'database'  => $_ENV['DB_USERS_NAME'],
+			'username'  => $_ENV['DB_USERS_USERNAME'],
+			'password'  => $_ENV['DB_USERS_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
