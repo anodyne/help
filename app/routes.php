048804b58c045ffd27b95876ed85859a0bcf53aa
David VanScott <david.vanscott@gmail.com>
2014-08-05 19:47:38 -0400

    Route file update

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', [
-	'as'	=> 'home',
-	'uses'	=> 'Help\Controllers\MainController@index']);
+Route::get('ratings', function()
+{
+	$a = ArticleModel::first();
+	s($a->getRating());
+});
