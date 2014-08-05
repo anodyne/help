eb0565bb801637c321eedec4547e8578d6d0ea76
David VanScott <david.vanscott@gmail.com>
2014-08-05 19:46:44 -0400

    Markdown service

---
+++ Anodyne/Help/Foundation/Facades/MarkdownFacade.php
@@ -0,0 +1,14 @@
+<?php namespace Help\Foundation\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class MarkdownFacade extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'markdown'; }
+
+}
