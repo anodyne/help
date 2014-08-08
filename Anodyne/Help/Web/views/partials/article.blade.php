00cc013e1a4a4c58818c7c3e52f5b8c5ac7bb7fc
David VanScott <david.vanscott@gmail.com>
2014-08-08 18:25:36 -0400

    Add the possibility of showing an article rating

--- Anodyne/Help/Web/views/partials/article.blade.php
+++ Anodyne/Help/Web/views/partials/article.blade.php
@@ -1,6 +1,9 @@
 <dt>{{ $article->present()->titleWithLink }}</dt>
 <dd>{{ $article->present()->summary }}</dd>
 <dd>
+	@if ($rating and $article->ratings->count() > 0)
+		{{ $article->present()->ratingLabel }}
+	@endif
 	{{ $article->present()->productLabel }}
 	{{ $article->present()->tagsLabel }}
 </dd>
