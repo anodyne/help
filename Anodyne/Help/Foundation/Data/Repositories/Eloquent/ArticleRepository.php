b814252e3e81d3f56b2bf063e972bec10908f4a1
David VanScott <david.vanscott@gmail.com>
2014-08-08 19:20:54 -0400

    More eager loading

--- Anodyne/Help/Foundation/Data/Repositories/Eloquent/ArticleRepository.php
+++ Anodyne/Help/Foundation/Data/Repositories/Eloquent/ArticleRepository.php
@@ -7,19 +7,19 @@ class ArticleRepository implements ArticleRepositoryInterface {
 
 	public function getByProduct($product)
 	{
-		return ArticleModel::with('tags', 'product')
+		return ArticleModel::with('tags', 'product', 'author', 'ratings')
 			->product($product)->orderBy('rating', 'desc')->get();
 	}
 
 	public function getPopular($number)
 	{
-		return ArticleModel::with('tags', 'product')
+		return ArticleModel::with('tags', 'product', 'author', 'ratings')
 			->orderBy('rating', 'desc')->take($number)->get();
 	}
 
 	public function getLatest($number)
 	{
-		return ArticleModel::with('tags', 'product')
+		return ArticleModel::with('tags', 'product', 'author', 'ratings')
 			->orderBy('created_at', 'desc')->take($number)->get();
 	}
 
