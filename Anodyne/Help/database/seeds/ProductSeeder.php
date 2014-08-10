044e4db0c79376db65d77f4a7dacdfd4383be28b
David VanScott <david.vanscott@gmail.com>
2014-08-10 13:36:08 -0400

    Make sure products get slugged

--- Anodyne/Help/database/seeds/ProductSeeder.php
+++ Anodyne/Help/database/seeds/ProductSeeder.php
@@ -10,11 +10,11 @@ class ProductSeeder extends Seeder {
 	public function run()
 	{
 		$products = [
-			['name' => 'Nova 1'],
-			['name' => 'Nova 2'],
-			['name' => 'Nova 3'],
-			['name' => 'AnodyneXtras'],
-			['name' => 'Anodyne Help Center'],
+			['name' => 'Nova 1', 'slug' => ''],
+			['name' => 'Nova 2', 'slug' => ''],
+			['name' => 'Nova 3', 'slug' => ''],
+			['name' => 'AnodyneXtras', 'slug' => ''],
+			['name' => 'Anodyne Help Center', 'slug' => ''],
 		];
 
 		foreach ($products as $product)
