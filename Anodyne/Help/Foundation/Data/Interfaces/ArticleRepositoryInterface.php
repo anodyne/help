5dc810553703e892f47529e3197dcc514b8ccc45
David VanScott <david.vanscott@gmail.com>
2014-08-10 13:36:37 -0400

    Article repo work

--- Anodyne/Help/Foundation/Data/Interfaces/ArticleRepositoryInterface.php
+++ Anodyne/Help/Foundation/Data/Interfaces/ArticleRepositoryInterface.php
@@ -2,7 +2,9 @@
 
 interface ArticleRepositoryInterface {
 
+	public function findByProductAndSlug($product, $slug);
 	public function getByProduct($product);
+	public function getComments($id);
 	public function getPopular($number);
 	public function getLatest($number);
 
--- Anodyne/Help/Foundation/Data/Repositories/Eloquent/ArticleRepository.php
+++ Anodyne/Help/Foundation/Data/Repositories/Eloquent/ArticleRepository.php
@@ -1,14 +1,26 @@
 <?php namespace Help\Foundation\Data\Repositories\Eloquent;
 
 use ArticleModel,
+	CommentModel,
 	ArticleRepositoryInterface;
 
 class ArticleRepository implements ArticleRepositoryInterface {
+
+	public function findByProductAndSlug($product, $slug)
+	{
+		return ArticleModel::with('tags', 'product', 'author', 'ratings', 'comments', 'comments.author')
+			->product($product)->slug($slug)->get();
+	}
 
 	public function getByProduct($product)
 	{
 		return ArticleModel::with('tags', 'product', 'author', 'ratings')
 			->product($product)->orderBy('rating', 'desc')->get();
+	}
+
+	public function getComments($id)
+	{
+		return CommentModel::with('author')->article($id)->get();
 	}
 
 	public function getPopular($number)
