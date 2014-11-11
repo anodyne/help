ff294a053546355a76585542c698666b7fb298af
David VanScott <david.vanscott@gmail.com>
2014-11-11 15:28:50 -0500

    Add anodyne config file

---
+++ app/config/anodyne.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	'links' => [
+
+		'www'		=> 'http://anodyne-productions.com/',
+		'nova'		=> 'http://anodyne-productions.com/nova/',
+		'forums'	=> 'http://forums.anodyne-productions.com/',
+		'xtras'		=> 'http://xtras.anodyne-productions.com/',
+
+	],
+
+];
