72b1aaf8684e3731fd1d8618ef9843768475c4aa
David VanScott <david.vanscott@gmail.com>
2014-11-26 14:51:50 -0500

    Article repo work

--- Anodyne/Help/Data/Interfaces/ArticleRepositoryInterface.php
+++ Anodyne/Help/Data/Interfaces/ArticleRepositoryInterface.php
@@ -1,13 +1,14 @@
 <?php namespace Help\Data\Interfaces;
 
-interface ArticleRepositoryInterface {
+interface ArticleRepositoryInterface extends BaseRepositoryInterface {
 
 	public function findByProductAndSlug($product, $slug);
 	public function getByProduct($product);
 	public function getComments($id);
 	public function getPopular($number);
 	public function getLatest($number);
-	public function search($term);
-	public function searchAdvanced(array $terms);
+	public function getByTag($tag);
+	public function search($input);
+	public function searchAdvanced(array $input);
 
 }
--- Anodyne/Help/Data/Repositories/ArticleRepository.php
+++ Anodyne/Help/Data/Repositories/ArticleRepository.php
@@ -4,18 +4,29 @@ use Article,
 	Comment,
 	ArticleRepositoryInterface;
 
-class ArticleRepository implements ArticleRepositoryInterface {
+class ArticleRepository extends BaseRepository implements ArticleRepositoryInterface {
+
+	protected $model;
+
+	public function __construct(Article $model)
+	{
+		$this->model = $model;
+	}
 
 	public function findByProductAndSlug($product, $slug)
 	{
-		return Article::with('tags', 'product', 'author', 'ratings', 'comments', 'comments.author')
-			->product($product)->slug($slug)->get();
+		return $this->make(['tags', 'product', 'author', 'ratings', 'comments', 'comments.author'])
+			->product($product)
+			->slug($slug)
+			->get();
 	}
 
 	public function getByProduct($product)
 	{
-		return Article::with('tags', 'product', 'author', 'ratings')
-			->product($product)->orderBy('rating', 'desc')->get();
+		return $this->make(['tags', 'product', 'author', 'ratings'])
+			->product($product)
+			->orderBy('rating', 'desc')
+			->get();
 	}
 
 	public function getComments($id)
@@ -25,46 +36,77 @@ class ArticleRepository implements ArticleRepositoryInterface {
 
 	public function getPopular($number)
 	{
-		return Article::with('tags', 'product', 'author', 'ratings')
-			->orderBy('rating', 'desc')->take($number)->get();
+		return $this->make(['tags', 'product', 'author', 'ratings'])
+			->orderBy('rating', 'desc')
+			->take($number)
+			->get();
 	}
 
 	public function getLatest($number)
 	{
-		return Article::with('tags', 'product', 'author', 'ratings')
-			->orderBy('created_at', 'desc')->take($number)->get();
-	}
-
-	public function search($term)
-	{
-		return Article::with('tags', 'product', 'author', 'ratings')
-			->where('title', 'like', "%{$term}%")
-			->orWhere('summary', 'like', "%{$term}%")
-			->orWhere('content', 'like', "%{$term}%")
+		return $this->make(['tags', 'product', 'author', 'ratings'])
+			->orderBy('created_at', 'desc')
+			->take($number)
 			->get();
 	}
 
+	public function getByTag($tag)
+	{
+		return $this->make(['tags', 'product', 'author', 'ratings'])
+			->tag($tag)
+			->orderBy('rating', 'desc')
+			->get();
+	}
+
+	/**
+	 * Search for an item by its name and/or description and paginate the
+	 * results.
+	 *
+	 * @param	string	$input
+	 * @return	Paginator
+	 */
+	public function search($input)
+	{
+		return $this->make(['tags', 'product', 'author', 'ratings'])
+			->where(function($query) use ($input)
+			{
+				$query->where('title', 'like', "%{$input}%")
+					->orWhere('summary', 'like', "%{$input}%")
+					->orWhere('content', 'like', "%{$input}%");
+			})->active()->paginate(25);
+	}
+
+	/**
+	 * Do an advanced search for items based on multiple criteria and paginate
+	 * the results.
+	 *
+	 * @param	array	$input
+	 * @return	Paginator
+	 */
 	public function searchAdvanced(array $input)
 	{
-		$search = ItemModel::query();
+		$search = Article::query()->with('product', 'type', 'user');
 
-		if (array_key_exists('type', $input) and count($input['type']) > 0)
+		if (array_key_exists('t', $input) and count($input['t']) > 0)
 		{
-			$search = $search->whereIn('type_id', $input['type']);
+			$search = $search->whereIn('type_id', $input['t']);
 		}
 
-		if (array_key_exists('product', $input) and count($input['product']) > 0)
+		if (array_key_exists('p', $input) and count($input['p']) > 0)
 		{
-			$search = $search->whereIn('product_id', $input['product']);
+			$search = $search->whereIn('product_id', $input['p']);
 		}
 
-		if ( ! empty($input['search']))
+		if ( ! empty($input['q']))
 		{
-			$search = $search->where('name', 'like', "%{$input['search']}%")
-				->orWhere('desc', 'like', "%{$input['search']}%");
+			$search = $search->where(function($query) use ($input)
+			{
+				$query->where('name', 'like', "%{$input['q']}%")
+					->orWhere('desc', 'like', "%{$input['q']}%");
+			});
 		}
 
-		return $search->get();
+		return $search->active()->paginate(25);
 	}
 
 }
