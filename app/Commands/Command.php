f67aac4f4de58f139fa56aea00f37b07bd81e6fd
David VanScott <david.vanscott@gmail.com>
2015-03-22 16:18:11 -0400

    Namespace change

--- app/Commands/Command.php
+++ app/Commands/Command.php
@@ -1,4 +1,4 @@
-<?php namespace App\Commands;
+<?php namespace Help\Commands;
 
 abstract class Command {
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
 	'Illuminate\Contracts\Http\Kernel',
-	'App\Http\Kernel'
+	'Help\Http\Kernel'
 );
 
 $app->singleton(
 	'Illuminate\Contracts\Console\Kernel',
-	'App\Console\Kernel'
+	'Help\Console\Kernel'
 );
 
 $app->singleton(
 	'Illuminate\Contracts\Debug\ExceptionHandler',
-	'App\Exceptions\Handler'
+	'Help\Exceptions\Handler'
 );
 
 /*
