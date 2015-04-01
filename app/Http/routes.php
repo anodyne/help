d449c76892052dc6451e7af7575bc9b45b498f1e
David VanScott <david.vanscott@gmail.com>
2015-04-01 09:05:28 -0400

    Route updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,34 +1,50 @@
 <?php
 
-Route::get('/', ['as' => 'home', 'uses' => 'MainController@index']);
+Route::get('/', [
+	'as'	=> 'home',
+	'uses'	=> 'MainController@index']);
 
-Route::get('login', ['as' => 'login', 'uses' => 'Auth\AuthController@getLogin']);
-Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
-Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('login', [
+	'as'	=> 'login',
+	'uses'	=> 'LoginController@index']);
+Route::get('logout', [
+	'as'	=> 'logout',
+	'uses'	=> 'LoginController@logout']);
+Route::post('login', 'LoginController@doLogin');
 
-Route::post('search', ['as' => 'search.do', 'uses' => 'MainController@foo']);
+Route::post('search', [
+	'as'	=> 'search.do',
+	'uses'	=> 'MainController@foo']);
 
-Route::get('product/{product}', ['as' => 'product', 'uses' => 'MainController@showProduct']);
-Route::get('tag/{tag}', ['as' => 'tag', 'uses' => 'MainController@showTag']);
-
-//Route::get('profile/{username}', ['as' => 'account.profile', 'uses' => 'MainController@foo']);
+Route::get('product/{product}', [
+	'as'	=> 'product',
+	'uses'	=> 'MainController@showProduct']);
+Route::get('tag/{tag}', [
+	'as'	=> 'tag',
+	'uses'	=> 'MainController@showTag']);
 
 $adminOptions = [
 	'prefix'		=> 'admin',
 	'middleware'	=> 'auth',
 ];
 
-Route::group([$adminOptions], function()
+Route::group($adminOptions, function()
 {
-	//Route::get('/', ['as' => 'admin', 'uses' => 'MainController@foo']);
+	Route::get('product/{id}/remove', 'Admin\ProductController@remove');
+	Route::post('product/set-slug', 'Admin\ProductController@setSlug');
+	Route::get('product/{id}/restore', 'Admin\ProductController@confirmRestore');
+	Route::put('product/{id}/restore', [
+		'as'	=> 'admin.product.restore',
+		'uses'	=> 'Admin\ProductController@restore']);
 
-	Route::get('admin/product/{id}/remove', 'Admin\ProductController@remove');
-	Route::post('admin/product/set-slug', 'Admin\ProductController@setSlug');
+	Route::get('tag/{id}/remove', 'Admin\TagController@remove');
+	Route::post('tag/set-slug', 'Admin\TagController@setSlug');
+	Route::get('tag/{id}/restore', 'Admin\TagController@confirmRestore');
+	Route::put('tag/{id}/restore', [
+		'as'	=> 'admin.tag.restore',
+		'uses'	=> 'Admin\TagController@restore']);
 
-	Route::get('admin/tag/{id}/remove', 'Admin\TagController@remove');
-	Route::post('admin/tag/set-slug', 'Admin\TagController@setSlug');
-
-	Route::resource('admin/product', 'Admin\ProductController', ['except' => ['show']]);
-	Route::resource('admin/tag', 'Admin\TagController', ['except' => ['show']]);
-	Route::resource('admin/article', 'Admin\ArticleController', ['except' => ['show']]);
+	Route::resource('product', 'Admin\ProductController', ['except' => ['show']]);
+	Route::resource('tag', 'Admin\TagController', ['except' => ['show']]);
+	Route::resource('article', 'Admin\ArticleController', ['except' => ['show']]);
 });
