c8b925eeecfea27b15a1f1e985153485189500d5
David VanScott <david.vanscott@gmail.com>
2015-04-01 09:05:05 -0400

    Updates to the auth stuff

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,17 +11,6 @@ class AuthController extends Controller {
 	protected $redirectTo			= '/';
 	protected $redirectAfterLogout	= '/';
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
-
 	use AuthenticatesAndRegistersUsers;
 
 	/**
