c4a53eeb5c217dba754b4423f792f6cce9bea7eb
David VanScott <david.vanscott@gmail.com>
2015-04-03 12:08:29 -0400

    Add the article transformer

---
+++ app/Data/Transformers/ArticleTransformer.php
@@ -0,0 +1,35 @@
+<?php namespace Help\Data\Transformers;
+
+use Article;
+use League\Fractal;
+
+class ArticleTransformer extends Fractal\TransformerAbstract {
+
+	public function transform(Article $article)
+	{
+		$tags = [];
+
+		foreach ($article->tags as $tag)
+		{
+			$tags[] = $tag->present()->name;
+		}
+
+		return [
+			'id'			=> (int) $article->id,
+			'title'			=> $article->present()->title,
+			'slug'			=> $article->present()->slug,
+			'summary'		=> $article->present()->summary,
+			'content'		=> $article->present()->content,
+			'author'		=> $article->present()->author,
+			'userId'		=> (int) $article->user_id,
+			'product'		=> $article->present()->product,
+			'productId'		=> (int) $article->product_id,
+			'isTrashed'		=> (bool) $article->trashed(),
+			'links'			=> [
+				'edit'		=> route('admin.article.edit', [$article->id]),
+			],
+			'tags'			=> (array) $tags,
+		];
+	}
+
+}
