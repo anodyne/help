751e802ba362e772cc3d843de0aa59de8ba42182
David VanScott <david.vanscott@gmail.com>
2015-04-04 12:26:17 -0400

    Updates to the article repo

--- app/Data/Interfaces/ArticleRepositoryInterface.php
+++ app/Data/Interfaces/ArticleRepositoryInterface.php
@@ -3,6 +3,10 @@
 interface ArticleRepositoryInterface extends BaseRepositoryInterface {
 
 	public function allWithTrashed(array $with = []);
+	public function checkForDuplicateSlugs($product, $slug);
+	public function create(array $data);
+	public function find($id);
 	public function getLatestArticles($number = 5);
+	public function update($id, array $data);
 
 }
--- app/Data/Repositories/ArticleRepository.php
+++ app/Data/Repositories/ArticleRepository.php
@@ -19,10 +19,81 @@ class ArticleRepository extends BaseRepository implements ArticleRepositoryInter
 		return $query->withTrashed()->get();
 	}
 
+	public function checkForDuplicateSlugs($product, $slug)
+	{
+		// Build the product repo
+		$productRepo = app('ProductRepository');
+
+		// Get the product
+		$product = $productRepo->find($product);
+
+		// Get all its articles
+		$articles = $productRepo->getProductArticles($product);
+
+		// Filter to just this slug
+		$articleCount = $articles->filter(function($a) use ($slug)
+		{
+			return $a->slug == $slug;
+		})->count();
+
+		if ($articleCount > 0) return true;
+
+		return false;
+	}
+
+	public function create(array $data)
+	{
+		// Create the article
+		$article = $this->model->create($data);
+
+		// Sync the tags
+		if (array_key_exists('tags', $data))
+		{
+			$article->tags()->sync($data['tags']);
+		}
+
+		return $article;
+	}
+
+	public function find($id)
+	{
+		$query = $this->make(['tags', 'product', 'author']);
+
+		return $query->where('id', '=', $id)->withTrashed()->first();
+	}
+
 	public function getLatestArticles($number = 5)
 	{
 		return $this->model->with(['tags', 'product', 'author'])
 			->latest()->limit($number)->get();
+	}
+
+	public function update($id, array $data)
+	{
+		// Get the article
+		$article = $this->find($id);
+
+		if ($article)
+		{
+			// Fill the article with the new info
+			$article->fill($data);
+
+			// Save the article
+			$article->save();
+
+			if (array_key_exists('tags', $data))
+			{
+				// Reset the tags
+				//$article->tags()->sync([]);
+
+				// Update the tags
+				$article->tags()->sync($data['tags']);
+			}
+
+			return $article;
+		}
+
+		return false;
 	}
 
 }
