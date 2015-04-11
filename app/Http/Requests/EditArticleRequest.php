b929b5d77f4c66663c18ea594997c8972e55725d
David VanScott <david.vanscott@gmail.com>
2015-04-11 15:27:40 -0400

    Fix some errors with editing articles

---
+++ app/Http/Requests/EditArticleRequest.php
@@ -0,0 +1,40 @@
+<?php namespace Help\Http\Requests;
+
+use Help\Http\Requests\Request;
+
+class EditArticleRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	public function rules()
+	{
+		return [
+			'product_id' => 'required|exists:products,id',
+			'title' => 'required',
+			'slug' => 'required',
+			'summary' => 'required',
+			'content' => 'required',
+		];
+	}
+
+	public function messages()
+	{
+		return [
+			'product_id.required' => "Please select a product for this article",
+			'product_id.exists' => "Please select a valid product",
+			'title.required' => "Please enter a title for this article",
+			'slug.required' => "Please enter a slug for this article",
+			'summary.required' => "Please enter a summary for this article",
+			'content.required' => "Please enter the content for this article",
+		];
+	}
+
+}
