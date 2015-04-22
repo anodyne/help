2894b81b9accc7789110734dd6c148a9f9525331
David VanScott <david.vanscott@gmail.com>
2015-04-22 13:42:36 -0400

    Use the env file for links

--- config/anodyne.php
+++ config/anodyne.php
@@ -15,10 +15,10 @@ return [
 
 	'links' => [
 
-		'www'		=> 'http://anodyne-productions.com/',
-		'nova'		=> 'http://anodyne-productions.com/nova',
-		'forums'	=> 'http://forums.anodyne-productions.com/',
-		'xtras'		=> 'http://xtras.anodyne-productions.com/',
+		'www'		=> env('LINKS_WWW', 'http://anodyne-productions.com/'),
+		'nova'		=> env('LINKS_NOVA', 'http://anodyne-productions.com/nova'),
+		'forums'	=> env('LINKS_FORUMS', 'http://forums.anodyne-productions.com/'),
+		'xtras'		=> env('LINKS_XTRAS', 'http://xtras.anodyne-productions.com/'),
 
 	],
 
